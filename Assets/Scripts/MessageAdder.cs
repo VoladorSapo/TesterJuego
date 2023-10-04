2ad09794602d1d5add1f90d7d1480da27ccd772c
VoladorSapo <voladorsapo@gmail.com>
2023-10-04 19:16:57 +0200

    Mensajes por tiempo y eso

--- Assets/Scripts/MessageAdder.cs
+++ Assets/Scripts/MessageAdder.cs
@@ -10,12 +10,19 @@ public class MessageAdder : MonoBehaviour
     int currentConversation;
     int rundown;
     public List<string> texts;
-    List<MessageClass> currentMessages;
+   List<List<MessageClass>> currentMessages;
     [SerializeField] private GameObject MessagePrefab;
         // Start is called before the first frame update
     void Start()
     {
+        currentMessages = new List<List<MessageClass>>();
         rundown = 0;
+        CloseBoard();
+        for (int i = 0; i < TextConversations.Count; i++)
+        {
+            currentMessages.Add(new List<MessageClass>());
+            TextConversations[i].GetComponent<CanvasGroup>().alpha = 0;
+        }
     }
 
     void AddMessage(string text, int type, int list)
@@ -28,15 +35,27 @@ public class MessageAdder : MonoBehaviour
     }
     void AddMessageList(MessageClass[] messages,int conversation)
     {
-        currentMessages.Clear();
-        currentMessages.AddRange(messages);
-        currentConversation = conversation;
-        AddMessage(currentMessages[0].text, currentMessages[0].type, conversation);
-        currentMessages.RemoveAt(0);
-        if (MessageBoard.activeSelf && TextConversations[conversation].activeSelf)
+        if ( currentMessages[conversation].Count == 0)
         {
-            IEnumerator courutine = AddAllMessages();
-            StartCoroutine(courutine);
+            currentMessages[conversation].AddRange(messages);
+            currentConversation = conversation;
+            AddMessage(currentMessages[conversation][0].text, currentMessages[conversation][0].type, conversation);
+            currentMessages[conversation].RemoveAt(0);
+            if (MessageBoard.GetComponent<CanvasGroup>().alpha == 1 && TextConversations[conversation].GetComponent<CanvasGroup>().alpha == 1)
+            {
+                IEnumerator courutine = AddAllMessages();
+                StartCoroutine(courutine);
+            }
+        }
+        else
+        {
+            currentMessages[conversation].AddRange(messages);
+            currentConversation = conversation;
+            if (MessageBoard.GetComponent<CanvasGroup>().alpha == 1 && TextConversations[conversation].GetComponent<CanvasGroup>().alpha == 1)
+            {
+                IEnumerator courutine = AddAllMessages();
+                StartCoroutine(courutine);
+            }
         }
     }
     int WaitTime(string text, int time, bool waitfixed)
@@ -47,24 +66,41 @@ public class MessageAdder : MonoBehaviour
     }
     IEnumerator AddAllMessages()
     {
-        foreach (MessageClass message in currentMessages)
+        yield return new WaitForSeconds(2);
+
+        foreach (MessageClass message in currentMessages[currentConversation])
         {
             AddMessage(message.text, message.type, currentConversation);
-            yield return new WaitForSeconds(WaitTime(message.text,message.time,message.waitTimeFixed));
-
+            // yield return new WaitForSeconds(WaitTime(message.text,message.time,message.waitTimeFixed));
+            yield return new WaitForSeconds(2);
         }
+        currentMessages[currentConversation].Clear();
     }
     public void OpenBoard()
     {
-        MessageBoard.SetActive(true);
-        TextConversations[currentConversation].SetActive(true);
-        AddAllMessages();
+        MessageBoard.GetComponent<CanvasGroup>().alpha = 1;
+        TextConversations[currentConversation].GetComponent<CanvasGroup>().alpha = 1;
+        IEnumerator courutine = AddAllMessages();
+        StartCoroutine(courutine);
 
+    }
+    public void ChangeConversation(int conver)
+    {
+        TextConversations[currentConversation].GetComponent<CanvasGroup>().alpha = 0;
+
+        currentConversation = conver;
+        MessageBoard.GetComponent<CanvasGroup>().alpha = 1;
+        TextConversations[currentConversation].GetComponent<CanvasGroup>().alpha = 1;
+        if (currentMessages[currentConversation].Count > 0)
+        {
+            IEnumerator courutine = AddAllMessages();
+            StartCoroutine(courutine);
+        }
     }
     public void CloseBoard()
     {
-        MessageBoard.SetActive(false);
-        TextConversations[currentConversation].SetActive(false);
+        MessageBoard.GetComponent<CanvasGroup>().alpha = 0;
+        TextConversations[currentConversation].GetComponent<CanvasGroup>().alpha = 0;
 
     }
     // Update is called once per frame
@@ -82,6 +118,14 @@ public class MessageAdder : MonoBehaviour
                 AddMessage(texts[i], i % 2 == 0 ? 0 : 1, 0);
 
             }
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            AddMessageList(new MessageClass[] { new MessageClass(0, 0, "Montate en mi motora", false), new MessageClass(1, 0, "Desayuna con huevo", false) , new MessageClass(0, 0, "Toma Mango", false) }, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            AddMessageList(new MessageClass[] { new MessageClass(0, 0, "*Se quita la ropa*", false), new MessageClass(1, 0, "*Le nalgea*", false) }, 1);
         }
     }
 }
