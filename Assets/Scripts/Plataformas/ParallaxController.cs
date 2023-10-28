0e3882327823195bac001e0b9321a0e4485034d9
Taleren <122693648+Taleren@users.noreply.github.com>
2023-10-29 01:29:07 +0200

    sacame la verga lupe que la traigo bien parallax

--- Assets/Scripts/Plataformas/ParallaxController.cs
+++ Assets/Scripts/Plataformas/ParallaxController.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParallaxController : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class ParallaxController : MonoBehaviour {
+    
+    private float length, startposx, startposy;
+    public GameObject cam;
+    public float parallaxEffect;
+    private float run = 0;
+
     void Start()
     {
-        
+        startposx = transform.position.x;
+        startposy = transform.position.y;
+        length = GetComponent<Renderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
         
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float distx = (cam.transform.position.x * parallaxEffect);
+        float disty = (cam.transform.position.y * parallaxEffect);
+
+        transform.position = new Vector3(startposx + distx,transform.position.y, transform.position.z);
+        
+       
+        
+        if (temp > startposx + length) startposx += length;
+        else if (temp < startposx - length) startposx -= length;
+ 
     }
 }
